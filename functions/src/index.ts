b09eee079764d484e11ea17605aa48e41790a4de
theprogrammergary <theprogrammergary@gmail.com>
2024-02-16 08:42:34 -0500

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
-import { onRequest } from "firebase-functions/v2/https";
 import * as express from "express";
-import { youtube } from "./controllers/youtube";
+import { onRequest } from "firebase-functions/v2/https";
+import { youtubeMethods } from "./services/youtube/controller";
 
 const app: express.Application = express();
 
-app.get("/youtube-notifications", youtube.get);
-app.post("/youtube-notifications", youtube.post);
+app.get("/youtube-notifications", youtubeMethods.get);
+app.post("/youtube-notifications", youtubeMethods.post);
 
 export const api = onRequest(app);
